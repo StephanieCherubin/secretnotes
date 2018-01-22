28288c91a6fb905b521005df2ad7a0408c92e6e3
Stephanie <seraphin.cherubin@gmail.com>
2018-01-22 15:41:59 -0500

    implemented authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  def current_user
+    #if current_user exists
+    #get the current user from the session hash, remember it in @current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_logged_in
+    return true if current_user
+
+    return redirect_to root_path
+    #go to the login screen if there is no current user
+  end
 end
